57ed56d4cd032606d628aed6bc9d5df27987afb4
Timothy Jeon <159089769+noobytim@users.noreply.github.com>
2024-11-16 20:58:48 -0500

    Update level1.js

--- UPLIFT_ELEVATOR_GAME_0.6/levels/level1.js
+++ UPLIFT_ELEVATOR_GAME_0.6/levels/level1.js
@@ -5,8 +5,8 @@ let levelConfigurations = {
     MAX_WEIGHT: 100,
     //timer: 30,
     passengers: [
-        { originFace: 'S', origin: 2, destination: 3, destinationFace: 'S', weight: 100 },
+        { originFace: 'S', origin: 2, destination: 3, destinationFace: 'N', weight: 100 },
         { originFace: 'E', origin: 4, destination: 2, destinationFace: 'E', weight: 50 },
-        { originFace: 'E', origin: 3, destination: 1, destinationFace: 'E', weight: 50 }
+        { originFace: 'W', origin: 3, destination: 1, destinationFace: 'N', weight: 50 }
     ]
 };
