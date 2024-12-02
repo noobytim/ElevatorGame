4c0bfd3698cf941ce14c27402be15090cd2a4fd0
Ren-Zhi Zheng <44009027+Ravioni@users.noreply.github.com>
2024-12-01 21:57:10 -0500

    Add files via upload

---
+++ UPLIFT_ELEVATOR_GAME_0.8/levels/level1.js
@@ -0,0 +1,12 @@
+// level1.js
+window.levelConfigurations = {
+    buildingHeight: 400,
+    floorTot: 4,
+    MAX_WEIGHT: 100,
+    timer: 15,
+    passengers: [
+        { originFace: 'S', origin: 2, destination: 3, destinationFace: 'S', weight: 100 },
+        { originFace: 'E', origin: 4, destination: 2, destinationFace: 'E', weight: 50 },
+        { originFace: 'E', origin: 3, destination: 1, destinationFace: 'E', weight: 50 }
+    ]
+};
